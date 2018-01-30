3ef76f403c1bdf0d44800c77374e997bd0b86102
jaswinder <jaswinder.wadali@gmail.com>
2018-01-30 15:45:29 +0530

    solve origin access bug

--- src/main/java/interview/artifacts/RestService.java
+++ src/main/java/interview/artifacts/RestService.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.interview.model.UserModel;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class RestService {
 
